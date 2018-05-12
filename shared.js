75f4b8dca2ccb90a536b9c848b7b2ac1a193a869
DESKTOP-G325OBC\Jeffr <jeffrey.goh@apptreme.sg>
2018-05-12 16:19:33 +0800

    edited for assignment purpose

--- shared.js
+++ shared.js
@@ -1,19 +1,34 @@
 let backdrop = document.querySelector('.backdrop');
 let modal = document.querySelector('.modal');
 let selectPlanButtons = document.querySelectorAll('.plan button');
-let modalNoButton = document.querySelector('button.modal__action--negative');
+let modalNoButton = document.querySelector('.modal__action--negative');
+let toggleButton = document.querySelector('.toggle-button');
+let mobileNav = document.querySelector('.mobile-nav');
 
 selectPlanButtons.forEach((i) => {
   i.addEventListener('click', () => {
-    modal.style.display = 'block';
-    backdrop.style.display = 'block';
+    modal.classList.add('open');
+    backdrop.classList.add('open');
   });
 });
 
-backdrop.addEventListener('click', closeModal);
-modalNoButton.addEventListener('click', closeModal);
+backdrop.addEventListener('click', () => {
+  mobileNav.classList.remove('open');
+  closeModal();
+});
+
+if (modalNoButton) {
+  modalNoButton.addEventListener('click', closeModal);
+}
 
 function closeModal() {
-  backdrop.style.display = 'none';
-  modal.style.display = 'none';
+  if(modal) {
+    modal.classList.remove('open');
+  }
+  backdrop.classList.remove('open');
 }
+
+toggleButton.addEventListener('click', () => {
+  mobileNav.classList.add('open');
+  backdrop.classList.add('open');
+});
